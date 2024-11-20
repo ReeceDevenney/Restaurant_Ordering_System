babace093801032640398475ec9f6a59d860a2e3
ReeceDevenney <reecedevenney@gmail.com>
2024-11-20 15:36:57 -0500

    split card info into number and expiration date

--- api/models/payment_information.py
+++ api/models/payment_information.py
@@ -8,7 +8,8 @@ class Payment_information(Base):
     __tablename__ = "Payment_information"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    cardInformation = Column(String(20), nullable=False)
+    cardNumber = Column(String(20), nullable=False)
+    cardExpirationDate = Column(DATETIME, nullable=False)
     transactionStatus = Column(String(15), nullable=False)
     paymentType = Column(String(10), nullable=False)
 
