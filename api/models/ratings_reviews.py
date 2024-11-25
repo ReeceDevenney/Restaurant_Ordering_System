a4051425272f8614dfe9550efda3d81439e1950e
Ethan Beeson <ethanmbeeson@gmail.com>
2024-11-25 15:23:59 -0500

    added the order_id and backpopulate relationship

--- api/models/ratings_reviews.py
+++ api/models/ratings_reviews.py
@@ -10,11 +10,14 @@ class Rating_Review(Base):
 
     review_id = Column(Integer, primary_key=True, index = True, autoincrement=True, nullable=False)
     user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False, unique=True)
+    order_id = Column(Integer, ForeignKey("orders.order_id"), nullable=False, unique=True)
     reviewer_name = Column(String(100))
     rating = Column(DECIMAL(precision=2, scale=1), nullable=False)
     review = Column(String(300))
 
+
     user = relationship("User", back_populates="reviews")
+    order = relationship("Order", back_populates="reviews")
 
     __table_args__ = (
         CheckConstraint('rating >= 0 AND rating <= 5', name='check_rating_range'),
